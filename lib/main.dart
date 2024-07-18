8f63fdbdbbb952197df1c96084df7d9ee0443406
kardelenkeles <kar_di44@hotmail.com>
2024-07-18 10:42:27 +0300

    .env integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:uplide_task/view/screens/home_screen.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
@@ -12,11 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: MyHomePage(),
+       home: HomePage(),
     );
   }
 }
